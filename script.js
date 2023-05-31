ef177a9b512218c251d9d4a8ebd98bb188cf144f
kim-ido <kimido987@gmail.com>
2023-05-31 11:44:40 +0900

    working on copy response button

--- script.js
+++ script.js
@@ -3,12 +3,51 @@ const sendButton = document.querySelector("#send-btn");
 const chatContainer = document.querySelector(".chat-container");
 
 let userText = null;
+const API_KEY = "";
 
 const createElement = (html, className) => {
     const chatDiv = document.createElement("div");
     chatDiv.classList.add("chat", className);
     chatDiv.innerHTML = html;
     return chatDiv;
+}
+
+const getChatResponse = async (incomingChatDiv) => {
+    const API_URL = "https://estsoft-openai-api.jejucodingcamp.workers.dev/";
+    const pElement = document.createElement("p");
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "text-davinci-003",
+            prompt: userText,
+            max_tokens: 2048,
+            temperature: 0.2,
+            n: 1,
+            stop: null
+        })
+    }
+
+    try {
+        const response = await(await fetch(API_URL, requestOptions)).json();
+        pElement.textContent = response.choices[0].text.trim();
+    } catch(error) {
+        console.log(error);
+    }
+
+    incomingChatDiv.querySelector(".typing-animation").remove();
+    incomingChatDiv.querySelector(".chat-details").appendChild(pElement);
+}
+
+const copyResponse = (copyBtn) => {
+    const responseTextElement = copyBtn.parentElement.querySelector("p");
+    navigator.clipboard.writeText(responseTextElement.textContent);
+    copyBtn.textContent = "done";
+    setTimeout(() => copyBtn.textContent = "content_copy", 1000);
 }
 
 const showTypingAnimation = () => {
@@ -21,23 +60,27 @@ const showTypingAnimation = () => {
                             <div class="typing-dot" style="--delay: 0.4s"></div>
                         </div>
                     </div>
-                    <span class="material-symbols-rounded">content_copy</span>
+                    <span onclick="copyResponse(this)" class="material-symbols-rounded">content_copy</span>
                 </div>`;
 
-    const outgoingChatDiv = createElement(html, "incoming");
-    chatContainer.appendChild(outgoingChatDiv);
+    const incomingChatDiv = createElement(html, "incoming");
+    chatContainer.appendChild(incomingChatDiv);
+    getChatResponse(incomingChatDiv);
 }
 
 const handleOutgoingChat = () => {
     userText = chatInput.value.trim();
+    if(!userText) return;
+
     const html = `<div class="chat-content">
                     <div class="chat-details">
                         <img src="images/user.png" alt="user-img" width="90px" height="90px">
-                        <p>${userText}</p>
+                        <p></p>
                     </div>
                 </div>`;
 
     const outgoingChatDiv = createElement(html, "outgoing");
+    outgoingChatDiv.querySelector("p").textContent = userText;
     chatContainer.appendChild(outgoingChatDiv);
     setTimeout(showTypingAnimation, 500);
 }
